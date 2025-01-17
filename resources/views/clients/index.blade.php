354eafe8a133278566d22af1394d855a1be0aa09
Erick Zelaya <eaclaya@Ericks-MacBook-Air.local>
2025-01-17 11:13:23 -0600

    Enable clients nav option on mobile

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,19 +1,18 @@
-
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Clients') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="flex justify-end items-end px-3">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+                <div class="p-2 text-gray-900 lg:p-6 dark:text-gray-100">
+                    <div class="flex items-end justify-end px-3">
                         <x-button-link href="{{ route('clients.create') }}" class>New Client</x-button-link>
                     </div>
-                    <livewire:client-table/>
+                    <livewire:client-table />
                 </div>
             </div>
         </div>
